884e84b7c2298a50884321aa8f189d11e0789c50
JanEltner <j.eltner@gmx.de>
2014-11-11 23:12:20 +0100

    Adds switching Fullscreen
    With a double click it is possible to switch between fullscreen or
    windowed

--- src/application/VideoPlayer.java
+++ src/application/VideoPlayer.java
@@ -6,12 +6,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import lib.DragAndDrop;
 import uiComponents.ControllBox;
 import uiComponents.SettingsBox;
@@ -24,10 +28,13 @@ public class VideoPlayer extends Application
 	private SpeedUpMoviePlayer player = new SpeedUpMoviePlayer(movie);
 	private MediaView mediaview = new MediaView(player.getPlayer());
 	private VBox vbox = new VBox();
+	private Scene scene;
 	protected Stage stage;
 	private BorderPane root;
 	private SettingsBox settingsBox;
 	private ControllBox controlBox;
+	
+	private boolean isFullscreen = false;
 	
 	@Override
 	public void start(Stage stage) 
@@ -44,14 +51,27 @@ public class VideoPlayer extends Application
 			root.getChildren().add(mediaview);
 			root.getChildren().add(vbox);
 			
-			Scene scene = new Scene(root,400,470,Color.BLACK);
+			this.scene = new Scene(root,400,470,Color.BLACK);
 			scene.getStylesheets().addAll(getClass().getResource("application.css").toExternalForm());
 			controlBox.initPlayButton(root);
 			root.requestFocus();
 			
+			
 			stage.setScene(scene);
 			stage.show();
 			
+			
+			root.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent mouseEvent) 
+				{
+					if(mouseEvent.getButton().equals(MouseButton.PRIMARY) && mouseEvent.getClickCount() == 2)
+					{
+						switchFullscreen();
+					}
+				}
+			});
 			
 			root.setOnKeyPressed(new EventHandler<KeyEvent>() {
 				
@@ -73,6 +93,36 @@ public class VideoPlayer extends Application
 		{
 			e.printStackTrace();
 		}
+	}
+	
+	private void switchFullscreen()
+	{
+		double height = 400;
+		double width = 470;
+		if(!this.isFullscreen)
+		{
+			Screen primaryScreen = Screen.getPrimary();
+			height = primaryScreen.getVisualBounds().getHeight();
+			width = primaryScreen.getVisualBounds().getWidth();
+		}
+		changeFullscreenHelper(height, width, !isFullscreen);
+		System.out.println(height + " - " + width );
+	}
+	
+	private void changeFullscreenHelper(double height, double width, boolean fullscreen)
+	{
+		stage.hide();
+		stage = new Stage();
+		stage.setScene(scene);
+		stage.setHeight(height);
+		stage.setWidth(width);
+		mediaview.fitHeightProperty().set(height);
+		mediaview.fitWidthProperty().set(width);
+		stage.setFullScreen(fullscreen);
+		stage.initStyle(fullscreen?StageStyle.UNDECORATED:StageStyle.DECORATED);
+		vbox.setTranslateY(height-90);
+		stage.show();
+		this.isFullscreen = fullscreen;
 	}
 	
 	public static void main(String[] args) 
